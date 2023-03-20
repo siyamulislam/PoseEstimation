ed7d0b93eb77277bda65f16782b89d3e207b31d0
siyamulislam <siyamulislam1@gmail.com>
2023-03-21 02:02:11 +0600

    curl with bar

--- AiTrainer.py
+++ AiTrainer.py
@@ -30,18 +30,29 @@ while True:
     # leftArm
     angle= detector.findAngle(img,11,13,15)
     per =np.interp(angle,(210,320),(0,100))
+    bar = np.interp(angle,(210,320),(650,100))
     # print(per)
 
     # check for the dumble curl
+    color = (255,0,255)
     if per ==100:
+        color = (0,255,0)
         if dir ==0:
             count+=0.5
             dir = 1
     if per == 0:
+        color = (0,0,255)
         if dir == 1:
             count+=0.5
             dir =0
     # print(count)
+
+    # bar draw
+    cv2.rectangle(img,(1100,100),(1175,650),color,4)
+    cv2.rectangle(img,(1100,int(bar)),(1175,650),color,cv2.FILLED)
+    cv2.putText(img,f'{int(per)}%',(1100,75),cv2.FONT_HERSHEY_PLAIN,4,color,4)
+
+    # curl count
     cv2.rectangle(img,(0,450),(250,720),(0,255,0),cv2.FILLED)
     cv2.putText(img,f'{int(count)}',(45,670),cv2.FONT_HERSHEY_PLAIN,15,(255,0,0),25)
 
